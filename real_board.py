ae3c1f4090369060df1f3ce613b5c35f4d633078
jakub-qak <84182822+jakub-qak@users.noreply.github.com>
2021-05-23 22:13:21 +0200

    Update real_board.py
    Fixed bug - now you can restart game even if someone win instead of just restarting in the middle of the game.
    Now you can also change dimensions of the board.

--- real_board.py
+++ real_board.py
@@ -3,15 +3,14 @@ import sys, pygame
 pygame.init()
 
 #dimensions
-WIDTH = 600
-HEIGHT = 600
-BOARD_ROWS = 3
-BOARD_COLS = 3
-CIRCLE_RADIUS = 60
+WIDTH = 300
+HEIGHT = 300
+SQUARES = WIDTH // 3
+CIRCLE_RADIUS = SQUARES // 3
 CIRCLE_WIDTH = 15
 CROSS_WIDTH = 25
 LINE_WIDTH = 10
-CROSS_CUTTING = 45
+CROSS_CUTTING = SQUARES // 4.5
 #colors
 CYAN = (0, 255, 255)
 LINE_COLOUR = (204, 93, 43)
@@ -40,10 +39,10 @@ def print_board(board):
 
 #Lines
 def draw_lines():
-    pygame.draw.line(screen, LINE_COLOUR, (40,200), (560,200), LINE_WIDTH)
-    pygame.draw.line(screen, LINE_COLOUR, (40,400), (560,400), LINE_WIDTH)
-    pygame.draw.line(screen, LINE_COLOUR, (200,40),(200,560), LINE_WIDTH)
-    pygame.draw.line(screen, LINE_COLOUR, (400,40),(400,560), LINE_WIDTH)
+    pygame.draw.line(screen, LINE_COLOUR, (0,SQUARES), (WIDTH,SQUARES), LINE_WIDTH)
+    pygame.draw.line(screen, LINE_COLOUR, (0,2*SQUARES), (WIDTH,2*SQUARES), LINE_WIDTH)
+    pygame.draw.line(screen, LINE_COLOUR, (SQUARES,0),(SQUARES,HEIGHT), LINE_WIDTH)
+    pygame.draw.line(screen, LINE_COLOUR, (2*SQUARES,0),(2*SQUARES,HEIGHT), LINE_WIDTH)
 
 draw_lines()
 
@@ -74,56 +73,56 @@ def win(player):
             colour = CROSS_COLOUR
         elif player == 2:
             colour = CIRCLE_COLOUR
-        pygame.draw.line(screen, colour, (0,500),(600,500), 20)
+        pygame.draw.line(screen, colour, (0,SQUARES*2.5),(SQUARES*3,SQUARES*2.5), 20)
         return True
     elif my_board["4"] == my_board["5"] == my_board["6"] != ' ':
         if player == 1:
             colour = CROSS_COLOUR
         elif player == 2:
             colour = CIRCLE_COLOUR
-        pygame.draw.line(screen, colour, (0,300),(600,300), 20)
+        pygame.draw.line(screen, colour, (0,SQUARES*1.5),(SQUARES*3,SQUARES*1.5), 20)
         return True
     elif my_board["7"] == my_board["8"] == my_board["9"] != ' ':
         if player == 1:
             colour = CROSS_COLOUR
         elif player == 2:
             colour = CIRCLE_COLOUR
-        pygame.draw.line(screen, colour, (0,100),(600,100), 20)
+        pygame.draw.line(screen, colour, (0,SQUARES*0.5),(SQUARES*3,SQUARES*0.5), 20)
         return True
     elif my_board["1"] == my_board["4"] == my_board["7"] != ' ':
         if player == 1:
             colour = CROSS_COLOUR
         elif player == 2:
             colour = CIRCLE_COLOUR
-        pygame.draw.line(screen, colour, (100,0),(100,600), 20)
+        pygame.draw.line(screen, colour, (SQUARES*0.5,0),(SQUARES*0.5,SQUARES*3), 20)
         return True
     elif my_board["2"] == my_board["5"] == my_board["8"] != ' ':
         if player == 1:
             colour = CROSS_COLOUR
         elif player == 2:
             colour = CIRCLE_COLOUR
-        pygame.draw.line(screen, colour, (300,0),(300,600), 20)
+        pygame.draw.line(screen, colour, (SQUARES*1.5,0),(SQUARES*1.5,SQUARES*3), 20)
         return True
     elif my_board["3"] == my_board["6"] == my_board["9"] != ' ':
         if player == 1:
             colour = CROSS_COLOUR
         elif player == 2:
             colour = CIRCLE_COLOUR
-        pygame.draw.line(screen, colour, (500,0),(500,600), 20)
+        pygame.draw.line(screen, colour, (SQUARES*2.5,0),(SQUARES*2.5,SQUARES*3), 20)
         return True
     elif my_board["7"] == my_board["5"] == my_board["3"] != ' ':
         if player == 1:
             colour = CROSS_COLOUR
         elif player == 2:
             colour = CIRCLE_COLOUR
-        pygame.draw.line(screen, colour, (0,0),(600,600), 20)
+        pygame.draw.line(screen, colour, (0,0),(SQUARES*3,SQUARES*3), 20)
         return True        
     elif my_board["9"] == my_board["5"] == my_board["1"] != ' ':
         if player == 1:
             colour = CROSS_COLOUR
         elif player == 2:
             colour = CIRCLE_COLOUR
-        pygame.draw.line(screen, colour, (0,600),(600,000), 20)
+        pygame.draw.line(screen, colour, (0,SQUARES*3),(SQUARES*3,0), 20)
         return True
 
     return False    
@@ -145,69 +144,68 @@ while True:
         if event.type == pygame.QUIT:
             sys.exit()
         
-        if event.type == pygame.MOUSEBUTTONDOWN: 
-            if not game_over:
+        if event.type == pygame.MOUSEBUTTONDOWN and not game_over: 
             
-                #mouse clicking
-                mouseX = event.pos[0] #x
-                mouseY = event.pos[1] #y
+            #mouse clicking
+            mouseX = event.pos[0] #x
+            mouseY = event.pos[1] #y
 
-                clicked_col = int(mouseX // 200) #x
-                clicked_row = int(mouseY // 200) #y
+            clicked_col = int(mouseX // SQUARES) #x
+            clicked_row = int(mouseY // SQUARES) #y
 
-                #coordinates of "O"
-                circle_x = clicked_col * 200 + 100
-                circle_y = clicked_row * 200 + 100
+            #coordinates of "O"
+            circle_x = clicked_col * SQUARES + SQUARES // 2
+            circle_y = clicked_row * SQUARES + SQUARES // 2
 
-                #coordinates of "X"
-                #first line
-                cross_first_line_start = (clicked_col*200 + CROSS_CUTTING ,clicked_row*200+200 - CROSS_CUTTING)
-                cross_first_line_end =  (clicked_col*200+200 - CROSS_CUTTING, clicked_row*200 + CROSS_CUTTING)
-                #second Line
-                cross_second_line_start = (clicked_col*200 + CROSS_CUTTING,clicked_row*200 + CROSS_CUTTING)
-                cross_second_line_end =  (clicked_col*200+200 - CROSS_CUTTING, clicked_row*200+200 - CROSS_CUTTING)
+            #coordinates of "X"
+            #first line
+            cross_first_line_start = (clicked_col*SQUARES + CROSS_CUTTING ,clicked_row*SQUARES+SQUARES - CROSS_CUTTING)
+            cross_first_line_end =  (clicked_col*SQUARES+SQUARES - CROSS_CUTTING, clicked_row*SQUARES + CROSS_CUTTING)
+            #second Line
+            cross_second_line_start = (clicked_col*SQUARES + CROSS_CUTTING,clicked_row*SQUARES + CROSS_CUTTING)
+            cross_second_line_end =  (clicked_col*SQUARES+SQUARES - CROSS_CUTTING, clicked_row*SQUARES+SQUARES - CROSS_CUTTING)
 
 
-                # moves on board = moves on console
-                move = [clicked_col, clicked_row]
-                
-                if move == [0,0]:
-                    move = "7"
-                elif move == [1,0]:
-                    move = "8"
-                elif move == [2,0]:
-                    move = "9"
-                elif move == [0,1]:
-                    move = "4"
-                elif move == [1,1]:
-                    move = "5"
-                elif move == [2,1]:
-                    move = "6"
-                elif move == [0,2]:
-                    move = "1"
-                elif move == [1,2]:
-                    move = "2"
-                elif move == [2,2]:
-                    move = "3"    
-                
-                # Changing player
-                if empty_space(move):
-                    if player == 1:
-                        my_board[move] = "0"
-                        player = 2
-                        if win(player):
-                            game_over = True
-                    elif player == 2:
-                        my_board[move] = "X"
-                        player = 1
-                        if win(player):
-                            game_over = True
-                    draw_figures(circle_x, circle_y, cross_first_line_start, cross_first_line_end, cross_second_line_start, cross_second_line_end)
+            # moves on board = moves on console
+            move = [clicked_col, clicked_row]
+            
+            if move == [0,0]:
+                move = "7"
+            elif move == [1,0]:
+                move = "8"
+            elif move == [2,0]:
+                move = "9"
+            elif move == [0,1]:
+                move = "4"
+            elif move == [1,1]:
+                move = "5"
+            elif move == [2,1]:
+                move = "6"
+            elif move == [0,2]:
+                move = "1"
+            elif move == [1,2]:
+                move = "2"
+            elif move == [2,2]:
+                move = "3"    
+            
+            # Changing player
+            if empty_space(move):
+                if player == 1:
+                    my_board[move] = "0"
+                    player = 2
+                    if win(player):
+                        game_over = True
+                elif player == 2:
+                    my_board[move] = "X"
+                    player = 1
+                    if win(player):
+                        game_over = True
+                draw_figures(circle_x, circle_y, cross_first_line_start, cross_first_line_end, cross_second_line_start, cross_second_line_end)
 
-                            
+                        
 
-                print(move)
-                print_board(my_board)
+            print(move)
+            print_board(my_board)
 
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_r:
@@ -216,6 +214,8 @@ while True:
                 my_board = {'7':' ','8':' ','9':' ',
          '4':' ','5':' ','6':' ',
          '1':' ','2':' ','3':' ',}
+                #Restart game even if someone win
+                game_over = False
             
             
     pygame.display.update()
